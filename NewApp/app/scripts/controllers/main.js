b6e501c9c954373b1e138e78fbe13b19f9f99732
Piyaratne <Piyaratne>
2016-01-21 11:20:39 +0530

    firstCommit

---
+++ NewApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newAppApp
+ */
+angular.module('newAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
